7072354b98ff844ddb1e089897e86a05ea6543ee
zhulinpinyu <mulixiang199016@gmail.com>
2016-08-03 17:42:10 +0800

    update style

--- src/App.js
+++ src/App.js
@@ -11,15 +11,10 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Welcome to React</h1>
+          <button type="button" className="btn btn-default btn-lg">
+            <span className="glyphicon glyphicon-star" aria-hidden="true"></span> Star
+          </button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <button type="button" className="btn btn-default btn-lg">
-          <span className="glyphicon glyphicon-star" aria-hidden="true"></span> Star
-        </button>
-
         <img src={docker} alt="docker"/>
       </div>
     );
